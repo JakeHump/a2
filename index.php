d770f9288683718b0a3ac472c14c9e48d17e23bd
Jake Humphrey <jakehumphrey@yahoo.com>
2017-02-20 22:46:03 -0500

    added css includes and index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Jake Humphrey</title>
+    <?php include("includes/htmlhead.php"); ?>
+</head>
+
+<body id="home">
+    <div class="container">
+        <div class="row">
+          <div class="twelve columns">
+            <header>
+              <h1>Scrabble Calculator</h1>
+                <img src="images/scrabble.jpg" alt="scrabble">
+              <h2>About Me</h2>
+              <p>
+                  I live in Long Beach, California.  I work in Bioinformatics and would like to improve my web site development skills.
+              </p>
+              <h3>Random Quote from Mark Twain</h3>
+              <p>
+                <?php include("includes/quotes.php"); ?>
+              </p>
+            </header>
+
+          </div>
+        </div>
+
+        <div class="row">
+            <?php include("includes/footer.php"); ?>
+        </div>
+
+    </div>
+</body>
+
+</html>
